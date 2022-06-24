2d7f18ca26c03240c3db97c759128fcf8dc83a93
Deepa2002 <97387384+Deepa2002@users.noreply.github.com>
2022-06-24 18:16:56 +0530

    commit

---
+++ LockedMe/src/Main.java
@@ -0,0 +1,14 @@
+
+
+
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "D:\\projects\\files"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.mainMenu();
+	}
+	
+}
